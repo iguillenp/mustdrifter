533e8aeb751e21d587c35036a4e782081735b698
iguillenp <ibaiguillenpacho@gmail.com>
2026-04-14 12:13:26 +0200

    fixed drift loaders

--- mustdrifter/mustdrifter.py
+++ mustdrifter/mustdrifter.py
@@ -177,25 +177,25 @@ class MuSTDrifter:
             return json.load(f)
 
     def load_syntax_style_drift(self, reference_period, test_period, metric):
-        _path = f"{self.syntax_style_drift_path}{reference_period}_{test_period}_{metric}.json"
+        _path = f"{self.syntax_style_drift_path}/{reference_period}_{test_period}_{metric}.json"
         self.logger.debug(f"Syntax style drift loaded from {_path}")
         with open(_path, "r", encoding="utf-8") as f:
             return json.load(f)
 
     def load_lexical_drift(self, reference_period, test_period, metric):
-        _path = f"{self.lexical_drift_path}{reference_period}_{test_period}_{metric}.json"
+        _path = f"{self.lexical_drift_path}/{reference_period}_{test_period}_{metric}.json"
         self.logger.debug(f"Lexical drift loaded from {_path}")
         with open(_path, "r", encoding="utf-8") as f:
             return json.load(f)
 
     def load_semantic_drift(self, reference_period, test_period, metric):
-        _path = f"{self.semantic_drift_path}{reference_period}_{test_period}_{metric}.json"
+        _path = f"{self.semantic_drift_path}/{reference_period}_{test_period}_{metric}.json"
         self.logger.debug(f"Semantic drift loaded from {_path}")
         with open(_path, "r", encoding="utf-8") as f:
             return json.load(f)
 
     def load_thematic_drift(self, reference_period, test_period, metric):
-        _path = f"{self.thematic_drift_path}{reference_period}_{test_period}_{metric}.json"
+        _path = f"{self.thematic_drift_path}/{reference_period}_{test_period}_{metric}.json"
         self.logger.debug(f"Thematic drift loaded from {_path}")
         with open(_path, "r", encoding="utf-8") as f:
             return json.load(f)
